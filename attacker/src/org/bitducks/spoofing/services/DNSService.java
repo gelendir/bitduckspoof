5ad1d0b70053ef3e35dc53c3098831e3c553bc62
Dalto <dalto.perreault@gmail.com>
2012-01-25 11:53:20 -0500

    Adding DNS Service

---
+++ attacker/src/org/bitducks/spoofing/services/DNSService.java
@@ -0,0 +1,28 @@
+package org.bitducks.spoofing.services;
+
+import jpcap.packet.Packet;
+import jpcap.packet.UDPPacket;
+
+import org.bitducks.spoofing.core.Service;
+import org.bitducks.spoofing.packet.DNSPacket;
+import org.bitducks.spoofing.packet.PacketFactory;
+
+public class DNSService extends Service {
+
+	@Override
+	public void run() {
+		UDPPacket queryPaquet = (UDPPacket)this.getNextPacket();
+		
+		/*DNSPacket answerPaquet = PacketFactory.dnsRequest(src_port, 
+				dst_port, 
+				ipSource, 
+				ipTarget, 
+				transactionId, 
+				questions, 
+				query, 
+				ipFalseDNS);*/
+		
+
+	}
+
+}
