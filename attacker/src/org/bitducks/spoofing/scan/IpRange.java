b78cb0c4ed9594fd47da18c7e48e7b71bc2d8b4d
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-01-25 10:21:34 -0500

    Add Ip range iterator

---
+++ attacker/src/org/bitducks/spoofing/scan/IpRange.java
@@ -0,0 +1,27 @@
+package org.bitducks.spoofing.scan;
+
+import java.net.InetAddress;
+import java.util.Iterator;
+
+public class IpRange implements Iterable<InetAddress> {
+	
+	byte[] start;
+	byte[] end;
+	
+	public IpRange(byte[] start, byte[] end) {
+		this.start = start;
+		this.end = end;
+	}
+	
+	public IpRange(InetAddress start, InetAddress end) {
+		this.start = start.getAddress();
+		this.end = end.getAddress();
+	}
+
+	@Override
+	public Iterator<InetAddress> iterator() {
+		IpRangeIterator iterator = new IpRangeIterator(this.start, this.end);
+		return iterator;
+	}
+
+}
