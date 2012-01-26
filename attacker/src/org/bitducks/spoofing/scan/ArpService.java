62f166f9bd39c1385ccd5bf4c8985ea6784e3ce9
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-01-26 05:22:55 -0500

    Add arp reply filtering to arp service

--- attacker/src/org/bitducks/spoofing/scan/ArpService.java
+++ attacker/src/org/bitducks/spoofing/scan/ArpService.java
@@ -1,20 +1,29 @@
 package org.bitducks.spoofing.scan;
 
+import java.util.HashMap;
+import java.util.Set;
+
+import jpcap.packet.ARPPacket;
 import jpcap.packet.Packet;
 
 import org.bitducks.spoofing.core.Rule;
 import org.bitducks.spoofing.core.Service;
+import org.bitducks.spoofing.core.rules.ARPResponseRule;
 import org.bitducks.spoofing.core.rules.ARPRule;
+import org.bitducks.spoofing.event.ARPEvent;
+import org.bitducks.spoofing.event.EventReciever;
 
 public class ArpService extends Service {
 	
 	ArpCache cache;
 	
+	HashMap<ARPEvent, Set<EventReciever>> eventRegistry;
+	
 	public ArpService() {
 		
 		this.cache = new ArpCache();
 		
-		Rule arpRule = new ARPRule();
+		Rule arpRule = new ARPResponseRule();
 		this.getPolicy().addRule(arpRule);
 		
 	}
@@ -22,16 +31,22 @@ public class ArpService extends Service {
 	public void run() {
 		
 		boolean run = true;
+		System.out.println("ARP service started");
 		
 		while( run ) {
 			
-			System.out.println("ARP service started");
+			ARPPacket packet = (ARPPacket)this.getNextPacket();
 			
-			Packet packet = this.getNextPacket();
-			System.out.println(packet);
+			byte[] targetMac = packet.target_hardaddr;
+			byte[] targetIp = packet.target_protoaddr;
+			this.cache.add(targetIp, targetMac);
 			
 		}
 		
 	}
-
+	
+	public ArpCache getCache() {
+		return this.cache;
+	}
+	
 }
