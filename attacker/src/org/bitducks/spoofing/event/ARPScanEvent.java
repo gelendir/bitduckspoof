1797f1f7044b8cc20b4d68d66748bfcec370ee5f
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-01-26 11:11:40 -0500

    Add Event messaging system

---
+++ attacker/src/org/bitducks/spoofing/event/ARPScanEvent.java
@@ -0,0 +1,11 @@
+package org.bitducks.spoofing.event;
+
+import java.net.InetAddress;
+
+public class ARPScanEvent extends ARPEvent {
+	
+	public ARPScanEvent(InetAddress[] addresses) {
+		
+	}
+
+}
