cc20487dfd3207567bc9e25ef13a28d16e280c2b
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-01-26 11:11:27 -0500

    Add ArpScanService

---
+++ attacker/src/org/bitducks/spoofing/scan/ArpScanService.java
@@ -0,0 +1,45 @@
+package org.bitducks.spoofing.scan;
+
+import java.net.InetAddress;
+
+import jpcap.NetworkInterface;
+import jpcap.NetworkInterfaceAddress;
+import jpcap.packet.ARPPacket;
+
+import org.bitducks.spoofing.core.Server;
+import org.bitducks.spoofing.core.Service;
+import org.bitducks.spoofing.packet.PacketGenerator;
+import org.bitducks.spoofing.util.IpUtil;
+
+
+public class ArpScanService extends Service {
+	
+	public ArpScanService() {
+		
+	}
+
+	@Override
+	public void run() {
+		
+		Server server = Server.getInstance();
+		
+		NetworkInterface device = server.getNetworkInterface();
+		NetworkInterfaceAddress deviceAddress = device.addresses[0];
+		
+		InetAddress start = IpUtil.network(deviceAddress);
+		InetAddress end = IpUtil.lastIpInNetwork(deviceAddress);
+		
+		IpRange ipRange = new IpRange(start, end);
+		
+		PacketGenerator generator = new PacketGenerator(device);
+		
+		for( InetAddress address: ipRange ) {
+			
+			ARPPacket arpRequest = generator.arpRequest(address);	
+			server.sendPacket(arpRequest);
+			
+		}
+		
+	}
+
+}
