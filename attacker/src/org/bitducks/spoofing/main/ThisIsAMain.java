81daf2b25ad25000f9a620b141f05712cae90181
ledor473 <ledor473@hotmail.com>
2012-01-31 15:24:35 -0500

    Modif on THisIsAMain

--- attacker/src/org/bitducks/spoofing/main/ThisIsAMain.java
+++ attacker/src/org/bitducks/spoofing/main/ThisIsAMain.java
@@ -14,7 +14,10 @@ public class ThisIsAMain {
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws IOException, InterruptedException {
-
+		Server.createInstance(JpcapCaptor.getDeviceList()[0]);
+		Server.getInstance().addService(new BroadcastARPService());
+		Thread.sleep(10000);
+		Server.getInstance().stopServer();
 	}
 
 }
