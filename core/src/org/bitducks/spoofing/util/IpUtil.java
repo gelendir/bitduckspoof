b45eab5802f7ef207e6f9e37eba00e8df6f55324
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-02-01 09:35:21 -0500

    Refactor IpUtils to use InterfaceInfo

--- core/src/org/bitducks/spoofing/util/IpUtil.java
+++ core/src/org/bitducks/spoofing/util/IpUtil.java
@@ -4,33 +4,21 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 
+import org.bitducks.spoofing.core.InterfaceInfo;
 import org.bitducks.spoofing.exception.UnexpectedErrorException;
 
+import jpcap.NetworkInterface;
 import jpcap.NetworkInterfaceAddress;
 
 public abstract class IpUtil {
 	
-	public static InetAddress network (NetworkInterfaceAddress address ) {
+	public static InetAddress network( NetworkInterfaceAddress address ) {
 		return IpUtil.network( address.address, address.subnet );
 	}
 	
-	public static InetAddress lastIpInNetwork( NetworkInterfaceAddress address ) {
-		return IpUtil.lastIpInNetwork( address.broadcast );
-	}
-	
-	public static InetAddress lastIpInNetwork2( NetworkInterfaceAddress address ) {
-		return IpUtil.lastIpInNetwork2( address.address, address.subnet );
-	}
-	
-	public static InetAddress lastIpInNetwork2( InetAddress address, InetAddress subnet ) {
-		byte[] network = address.getAddress();
-		byte[] mask = subnet.getAddress();
-		
-		for( int i = 0; i < network.length; ++i ) {
-			network[i] = (byte) ((network[i] & mask[i]) | (mask[i] ^ (byte)0xff));
-		}
-		
-		return IpUtil.lastIpInNetwork(IpUtil.bytesToInet(network));
+	public static InetAddress network( NetworkInterface device ) {
+		InterfaceInfo info = new InterfaceInfo( device );
+		return IpUtil.network( info.getAddress(), info.getSubnet() );
 	}
 	
 	public static InetAddress network( InetAddress address, InetAddress subnet ) {
@@ -46,6 +34,15 @@ public abstract class IpUtil {
 		
 	}
 	
+	public static InetAddress lastIpInNetwork( NetworkInterfaceAddress address ) {
+		return IpUtil.lastIpInNetwork( address.broadcast );
+	}
+	
+	public static InetAddress lastIpInNetwoek( NetworkInterface device  ) {
+		InterfaceInfo info = new InterfaceInfo( device );
+		return IpUtil.lastIpInNetwork( info.getSubnet() );
+	}
+	
 	public static InetAddress lastIpInNetwork( InetAddress broadcast ) {
 		
 		int rawBroadcast = ByteBuffer.wrap(broadcast.getAddress()).getInt();
@@ -56,6 +53,21 @@ public abstract class IpUtil {
 		
 		return IpUtil.bytesToInet(buffer.array());
 		
+	}
+	
+	public static InetAddress lastIpInNetwork2( NetworkInterfaceAddress address ) {
+		return IpUtil.lastIpInNetwork2( address.address, address.subnet );
+	}
+	
+	public static InetAddress lastIpInNetwork2( InetAddress address, InetAddress subnet ) {
+		byte[] network = address.getAddress();
+		byte[] mask = subnet.getAddress();
+		
+		for( int i = 0; i < network.length; ++i ) {
+			network[i] = (byte) ((network[i] & mask[i]) | (mask[i] ^ (byte)0xff));
+		}
+		
+		return IpUtil.lastIpInNetwork(IpUtil.bytesToInet(network));
 	}
 	
 	public static InetAddress bytesToInet( byte[] address ) {
