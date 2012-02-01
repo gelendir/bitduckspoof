92410d3a9181e8b090547fe625e547188782863a
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-02-01 10:17:16 -0500

    Implement Timer for starting scans

---
+++ attacker/src/org/bitducks/spoofing/scan/ArpScanTimer.java
@@ -0,0 +1,51 @@
+package org.bitducks.spoofing.scan;
+
+import java.net.InetAddress;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.TimerTask;
+
+import org.bitducks.spoofing.exception.UnexpectedErrorException;
+
+public class ArpScanTimer extends TimerTask {
+	
+	private Collection<InetAddress> addresses;
+	private ArpScanService arpScan;
+	private ArpRecieveService reciever;
+	private ArpScanFinish finish;
+
+	public ArpScanTimer( Collection<InetAddress> addresses, 
+			ArpScanService arpScan, 
+			ArpRecieveService reciever,
+			ArpScanFinish finish ) {
+		this.addresses = addresses;
+		this.arpScan = arpScan;
+		this.reciever = reciever;
+		this.finish = finish;
+		
+	}
+
+	@Override
+	public void run() {
+		
+		this.arpScan.runScan( this.addresses );
+		
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			throw new UnexpectedErrorException( e, "error while sleeping in ArpScanTimer" );
+		}
+		
+		LinkedList<InetAddress> noResponse = new LinkedList<InetAddress>();
+		
+		for( InetAddress address: this.addresses ) {
+			if( !this.reciever.getCache().hasAddress( address ) ) {
+				noResponse.add( address );
+			}
+		}
+		
+		finish.scanFinished( noResponse );
+		
+	}
+
+}
