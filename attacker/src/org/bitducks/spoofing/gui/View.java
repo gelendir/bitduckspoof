34d7c59466d3b4fc7244964386fa02b520191f4d
Dalto <dalto.perreault@gmail.com>
2012-02-06 14:44:19 -0500

    UI working

--- attacker/src/org/bitducks/spoofing/gui/View.java
+++ attacker/src/org/bitducks/spoofing/gui/View.java
@@ -4,7 +4,13 @@ import javax.swing.JPanel;
 
 public class View extends JPanel {
 	
+	protected String title = "";
+	
 	public View() {
 		
+	}
+	
+	public String getTitle() {
+		return this.title;
 	}
 }
