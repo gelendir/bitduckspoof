a005bf1aa14b19224e123171b5be410335a48e4d
Frédérik Paradis <fredy_14@live.fr>
2012-02-06 13:24:39 -0500

    move of dhcp4java

---
+++ defender/src/org/bitducks/spoofing/services/RogueDHCPDetectionService.java
@@ -0,0 +1,13 @@
+package org.bitducks.spoofing.services;
+
+import org.bitducks.spoofing.core.Service;
+
+public class RogueDHCPDetectionService extends Service {
+
+	@Override
+	public void run() {
+		
+
+	}
+
+}
