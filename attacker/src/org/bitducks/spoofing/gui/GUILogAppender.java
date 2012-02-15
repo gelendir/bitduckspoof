3c1f830c87c4ede61363ef0ca55fc400846bdbfc
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-02-15 11:20:20 -0500

    Add default level to GUI log appender

--- attacker/src/org/bitducks/spoofing/gui/GUILogAppender.java
+++ attacker/src/org/bitducks/spoofing/gui/GUILogAppender.java
@@ -4,23 +4,28 @@ import javax.swing.JTextArea;
 
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Layout;
+import org.apache.log4j.Level;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.spi.LoggingEvent;
 
 public class GUILogAppender extends AppenderSkeleton {
 	
 	private static Layout defaultLayout = new PatternLayout(PatternLayout.TTCC_CONVERSION_PATTERN);
+	private static Level defaultLevel = Level.INFO;
 	
 	private JTextArea logView;
 	boolean active = true;
 
-	public GUILogAppender( JTextArea logView, Layout layout ) {
+	private Level level;
+
+	public GUILogAppender( JTextArea logView, Layout layout, Level level ) {
 		this.logView = logView;
 		this.layout = layout;
+		this.level = level;
 	}
 	
 	public GUILogAppender( JTextArea logView ) {
-		this( logView, GUILogAppender.defaultLayout );
+		this( logView, GUILogAppender.defaultLayout, GUILogAppender.defaultLevel );
 	}
 
 	@Override
@@ -35,7 +40,7 @@ public class GUILogAppender extends AppenderSkeleton {
 
 	@Override
 	protected void append(LoggingEvent event) {
-		if( this.active ) {
+		if( this.active && event.getLevel().isGreaterOrEqual( this.level ) ) {
 			String message = this.getLayout().format( event );
 			this.logView.append( message );
 		}
