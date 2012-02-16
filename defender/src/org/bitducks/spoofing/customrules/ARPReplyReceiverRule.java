30351cbf65ca171cfb086fc51410238a3a65f8dc
Frédérik Paradis <fredy_14@live.fr>
2012-02-16 10:42:40 -0500

    javadoc

--- defender/src/org/bitducks/spoofing/customrules/ARPReplyReceiverRule.java
+++ defender/src/org/bitducks/spoofing/customrules/ARPReplyReceiverRule.java
@@ -6,10 +6,25 @@ import jpcap.packet.ARPPacket;
 import jpcap.packet.Packet;
 
 import org.bitducks.spoofing.core.rules.ARPRule;
-
+/**
+ * A rule to receive the reply from a know host.
+ */
+/**
+ * This class is a rule to receive the reply from a known host.
+ * @author Frédérik Paradis
+ */
 public class ARPReplyReceiverRule extends ARPRule {
+	
+	/**
+	 * The IP address to filter.
+	 */
 	private InetAddress addr;
 	
+	/**
+	 * This method verify if the packet passed in
+	 * parameter is an ARP reply and if the sender address
+	 * is from the IP address set with {@link ARPReplyReceiverRule#setInetAddress(InetAddress)}
+	 */
 	@Override
 	public boolean checkRule(Packet p) {
 		if(super.checkRule(p) && this.addr != null) {
@@ -21,6 +36,10 @@ public class ARPReplyReceiverRule extends ARPRule {
 		return false;
 	}
 	
+	/**
+	 * This method modify the IP address to filter.
+	 * @param addr The IP address to filter.
+	 */
 	public void setInetAddress(InetAddress addr) {
 		this.addr = addr;
 	}
