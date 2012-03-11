fc62602b34c247401f369b4a5aea25749507568d
Dalto <dalto.perreault@gmail.com>
2012-03-11 19:08:10 -0400

    Added interval option to the interface

--- defender/src/org/bitducks/spoofing/gui/ARPReplyRateServiceView.java
+++ defender/src/org/bitducks/spoofing/gui/ARPReplyRateServiceView.java
@@ -1,19 +1,39 @@
 package org.bitducks.spoofing.gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 import org.bitducks.spoofing.core.Service;
 import org.bitducks.spoofing.services.ARPReplyRateService;
 
-public class ARPReplyRateServiceView extends View {
+public class ARPReplyRateServiceView extends View implements ActionListener {
 
+	
+	public static final String CHANGE_INTERVAL = "Change DNS";
+	
+	public static final String BEGIN_TEXT_INTERVAL = "Interval : ";
+	
+	public static final String ENTER_INTERVAL = "Enter the inverval you want the service to check for ARP.";
+	public static final String ERROR_WRONG_NUMBER = "Invalid operation: The number you have provided is not valid.";
+	
 	final public static String TITLE = "ARP Reply Rate";
-	final public static String EXPLANATION = "ARP Reply Rate is a detector of ARP " +
-			"spoofing. It calculates the rate between the number of the ARP replies " +
-			"and ARP requests received. If there is more than one ARP reply for an " +
-			"IP address, this means that this IP address may be spoofed.";
+	final public static String EXPLANATION = "<html>ARP Reply Rate is a detector of ARP<br /> " +
+			"spoofing. It calculates the rate between the number of the ARP replies<br /> " +
+			"and ARP requests received. If there is more than one ARP reply for an<br /> " +
+			"IP address, this means that this IP address may be spoofed.<br />";
+	
+	private JLabel intervalLabel = new JLabel();
+	private JButton changeInterval = new JButton();
+	
+	private int interval = 10;
+	private ARPReplyRateService service = null;
 	
 	public ARPReplyRateServiceView() {
 		super( ARPReplyRateServiceView.TITLE );
@@ -25,15 +45,78 @@ public class ARPReplyRateServiceView extends View {
 	
 		this.servicePanel.setLayout( new BorderLayout() );
 			
-		JLabel descLabel = new JLabel(ARPReplyRateServiceView.EXPLANATION );
+		// Text
+		setupText();
 		
-		this.servicePanel.add( descLabel, BorderLayout.CENTER );
+		// Button
+		setupButton();
+	}
+	
+	private void setupText() {
+		JPanel tmpPan = new JPanel();
+		tmpPan.setLayout(new BoxLayout(tmpPan, BoxLayout.Y_AXIS));
 			
+		JLabel descLabel = new JLabel( ARPReplyRateServiceView.EXPLANATION );
+		this.intervalLabel.setText(ARPReplyRateServiceView.BEGIN_TEXT_INTERVAL + this.interval);
+		
+		tmpPan.add( descLabel );
+		tmpPan.add( this.intervalLabel );
+		
+		this.servicePanel.add(tmpPan, BorderLayout.CENTER);
+	}
+	
+	private void setupButton() {
+		JPanel tmpPan = new JPanel();
+		tmpPan.setLayout(new BoxLayout(tmpPan, BoxLayout.Y_AXIS));
+		
+		this.changeInterval.setText(ARPReplyRateServiceView.CHANGE_INTERVAL);
+		this.changeInterval.addActionListener(this);
+		this.changeInterval.setActionCommand(ARPReplyRateServiceView.CHANGE_INTERVAL);
+		
+		tmpPan.add( this.changeInterval );
+		
+		this.servicePanel.add(tmpPan, BorderLayout.LINE_END);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		String retval = "";
+		if ((retval = JOptionPane.showInputDialog(ARPReplyRateServiceView.ENTER_INTERVAL, this.interval + ""))
+				== null) {
+			// If cancel is called
+			return;
+		}
+		
+		int intRetval;
+		try {
+			intRetval = Integer.parseInt(retval);
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(null, ARPReplyRateServiceView.ERROR_WRONG_NUMBER);
+			return;
+		}
+		
+		if (intRetval <= 0)
+		{
+			JOptionPane.showMessageDialog(null, ARPReplyRateServiceView.ERROR_WRONG_NUMBER);
+			return;
+		}
+		
+		// Setting the new interval
+		this.interval = intRetval;
+		if (service != null)
+		{
+			service.setInterval(this.interval);
+		}
+		
+		this.intervalLabel.setText(ARPReplyRateServiceView.BEGIN_TEXT_INTERVAL + this.interval);
 	}
 	
 	@Override
 	protected Service createService() {
-		return new ARPReplyRateService(10);
+		service = new ARPReplyRateService(this.interval);
+		
+		return service;
 	}
 
 }
